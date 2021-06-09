f03655542b10734e097fc1294795a3f99bb12c40
Tonny <mrtonnysu@gmail.com>
2021-06-09 18:01:12 +0200

    Update SearchFragment.kt

--- app/src/main/java/tech/tucano/pmp_politie/UI/Main/SearchFragment.kt
+++ app/src/main/java/tech/tucano/pmp_politie/UI/Main/SearchFragment.kt
@@ -102,25 +102,17 @@ class SearchFragment: Fragment() {
                 for (snapshot: DataSnapshot in dataSnapshot.children) {
                     val article = Article(snapshot)
                     try {
-                        if (article.classified) {
-                            if (article.classified && (mPrefs.getString(getString(R.string.guest),
-                                    false.toString()) == false.toString()
-                                        )
+                        if (article.articleTitle!!.toLowerCase(Locale.ROOT)
+                                .contains(searchText.toLowerCase())
+                        ) {
+                            if (article.classified && mPrefs.getBoolean(getString(R.string.guest),
+                                    true)
                             ) {
-                                suggestions.add(
-                                    SearchModel(
-                                        article.articleTitle!!,
-                                        article.id
-                                    )
-                                )
+                                setupSearch(article, suggestions)
+                            } else if (!article.classified){
+                                setupSearch(article, suggestions)
                             }
-                        } else {
-                            suggestions.add(
-                                SearchModel(
-                                    article.articleTitle!!,
-                                    article.id
-                                )
-                            )
+                            Log.v("TAG", Article(snapshot).articleTitle!!)
                         }
                     } catch (e: Exception) {
                         e.printStackTrace()
@@ -136,14 +128,14 @@ class SearchFragment: Fragment() {
         })
     }
 
-//     fun setupSearch(article: Article, suggestions: ArrayList<SearchModel> = ArrayList()) {
-//         suggestions.add(
-//             SearchModel(
-//                 article.articleTitle!!,
-//                 article.id
-//             )
-//         )
-//     }
+    fun setupSearch(article: Article, suggestions: ArrayList<SearchModel> = ArrayList()) {
+        suggestions.add(
+            SearchModel(
+                article.articleTitle!!,
+                article.id
+            )
+        )
+    }
 
     fun Fragment.hideKeyboard() {
         view?.let { activity?.hideKeyboard(it) }
